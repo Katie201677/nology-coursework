f3d7069a202014a97c0fb06d8d1e909a542e21c6
Katie Walker <katiewalker0517@gmail.com>
2021-05-07 10:26:00 +0100

    styles added to vegetable price checker

--- 03-JavaScriptFundamentals/Control-Flow&Types-Challenge/vegetable-prices/script.js
+++ 03-JavaScriptFundamentals/Control-Flow&Types-Challenge/vegetable-prices/script.js
@@ -1,22 +1,22 @@
 function handleChange(event) {
   const vegetable = event.target.value;
-  const para = document.querySelector(".priceDisplay");
+  const para = document.querySelector(".main-box__priceDisplay");
   let price;
 
   switch(vegetable) {
     case "potatoes":
     case "carrots":
-      price = "£0.50 per kilo";
+      price = "£0.50";
       break;
     case "brocolli":
-      price = "£1.20 per kilo";
+      price = "£1.20";
       break;
     case "cabbage":
-      price = "£0.20 per kilo";
+      price = "£0.20";
       break;
     case "asparagus":
-      price = "£2.20 per kilo";
+      price = "£2.20";
       break;
   }
-  para.textContent =`${vegetable} price is ${price}`;
+  para.textContent =`Price per kilo: ${price}`;
 }
